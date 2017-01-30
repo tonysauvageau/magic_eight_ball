dfc4ea2fa83470763c867a7257fa7562ba28830f
tonysauvageau <toneeees@gmail.com>
2017-01-29 19:29:51 -0700

    added notifications, stylesheet work, still need to work on clearing answers.

--- main.js
+++ main.js
@@ -1,11 +1,76 @@
+var notifications = document.getElementById("notifications");
+var theNotification = '';
+
+    var defaultAnswers = ["Ask Me If I Care","Dumb Question Ask Another", "Forget About It", "Get A Clue", "In Your Dreams",  "Not A Chance", "Obviously", "Oh Please", "Sure", "Well Maybe", "What Do You Think?", "Whatever", "Who Cares?","Yeah And I'm The Pope", "Yeah Right", "You Wish", "You've Got To Be Kidding..."];
+    var newAnswers = [],
+        new_answers = [];
+
 function shakeBall()
 {
+    if (newAnswers.length == 0 || newAnswers.length <= defaultAnswers.length)
+    {
+        console.log(newAnswers.length);
+        var theMessages = defaultAnswers;
+        console.log(theMessages);
+
+    } else if (newAnswers.length > defaultAnswers.length) {
+        var theMessages = newAnswers;
+        console.log("New answers\n"+theMessages);
+    }
+
     var theMessageContainer = document.getElementById('eight-ball-message');
+    var theQuestion = document.getElementById('the-question').value;
 
-    var theMessages = ["Ask Me If I Care","Dumb Question Ask Another", "Forget About It", "Get A Clue", "In Your Dreams",  "Not A Chance", "Obviously", "Oh Please", "Sure", "Well Maybe", "What Do You Think?", "Whatever", "Who Cares?","Yeah And I'm The Pope", "Yeah Right", "You Wish", "You've Got To Be Kidding..."];
+    var add_answer = theQuestion.includes("add_answer:");
+    var clear_answer = theQuestion.includes("clear_answers");
+    var clear_history = theQuestion.includes("clear_history");
 
-    var randomNumber = Math.random() * (theMessages.length - 0) + 0;
+    var randomNumber = Math.round(Math.random() * (theMessages.length - 0) + 0);
 
-    theMessageContainer.innerHTML = theMessages[Math.round(randomNumber)]
+    if(add_answer)
+    {
+        theNotification = '<span class="green-text"><small><i>Added new answer: <b class="new_answer">'+theQuestion.replace('add_answer:','')+'</b></i></small></span>';
+        if(newAnswers.length == 0)
+        {
+            newAnswers = defaultAnswers;
+        }
+        newAnswers.push(theQuestion.replace('add_answer: ',''));
+        new_answers.push(theQuestion.replace('add_answer: ',''));
+        addNotification(theNotification);
+    } 
+    else if(clear_answer) {
 
+        theNotification = '<span class="green-text"><small><i>Cleared new answers</i></small></span>';
+        addNotification(theNotification);
+        newAnswers = [];
+        new_answers = [];
+        theMessages = defaultAnswers;
+
+    }else if(clear_history){
+
+        notifications.innerHTML= '';
+        theNotification = '<span class="green-text"><small><i>Cleared history</i></small></span>';
+        addNotification(theNotification);
+
+    } else {
+
+        theNotification = '<span><small><i>'+theQuestion+'</i><br/><b>'+theMessages[randomNumber]+'</b></small></span>';
+        addNotification(theNotification);
+        theMessageContainer.innerHTML = theMessages[randomNumber];
+    }
+
+    
+    document.getElementById('the-question').value = null;
 }
+
+function addNotification(notif)
+{
+   notifications.innerHTML += notif+'</br><br/>';
+}
+
+document.getElementById("the-question").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if ( event.keyCode === 13 ) {
+        document.getElementById("shake-button").click();
+    }
+});
